7f8371b59311c49a92ec21d78edba9d9e0b9c259
btoarriola <betokanon@gmail.com>
2023-05-25 16:07:13 -0600

    nuevo new

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -41,17 +41,17 @@ for msg in consumer:
 
     # Create dictionary and ingest data into MongoDB
     try:
-       agg_result= db.memes_info.aggregate(
+       agg_result= db.tkdapp_info.aggregate(
        [{
          "$group" : 
          {  "_id" : "$name", 
             "n"    : {"$sum": 1}
          }}
        ])
-       db.memes_summary.delete_many({})
+       db.tkdapp_summary.delete_many({})
        for i in agg_result:
          print(i)
-         summary_id = db.memes_summary.insert_one(i)
+         summary_id = db.tkdapp_summary.insert_one(i)
          print("Summary inserted with record ids", summary_id)
 
     except Exception as e:
